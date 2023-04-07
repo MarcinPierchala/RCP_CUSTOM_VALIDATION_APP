1c13fcd633aaaceaee74c4721681d81c08d12d30
MarcinPierchala <68757012+MarcinPierchala@users.noreply.github.com>
2023-04-07 10:14:34 +0200

    first_official_upload

---
+++ WORK_TIME_CONTROL.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace WORK_TIME_CTRL
+{
+    partial class formMainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.iMPORTNEWGRIDToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hELPToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aBOUTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eXITToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.iMPORTNEWGRIDToolStripMenuItem,
+            this.hELPToolStripMenuItem,
+            this.eXITToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1484, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // iMPORTNEWGRIDToolStripMenuItem
+            // 
+            this.iMPORTNEWGRIDToolStripMenuItem.Name = "iMPORTNEWGRIDToolStripMenuItem";
+            this.iMPORTNEWGRIDToolStripMenuItem.Size = new System.Drawing.Size(119, 20);
+            this.iMPORTNEWGRIDToolStripMenuItem.Text = "IMPORT NEW GRID";
+            this.iMPORTNEWGRIDToolStripMenuItem.Click += new System.EventHandler(this.btopen_Click);
+            // 
+            // hELPToolStripMenuItem
+            // 
+            this.hELPToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aBOUTToolStripMenuItem});
+            this.hELPToolStripMenuItem.Name = "hELPToolStripMenuItem";
+            this.hELPToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.hELPToolStripMenuItem.Text = "HELP";
+            // 
+            // aBOUTToolStripMenuItem
+            // 
+            this.aBOUTToolStripMenuItem.Name = "aBOUTToolStripMenuItem";
+            this.aBOUTToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.aBOUTToolStripMenuItem.Text = "ABOUT";
+            this.aBOUTToolStripMenuItem.Click += new System.EventHandler(this.AboutMe_Click);
+            // 
+            // eXITToolStripMenuItem
+            // 
+            this.eXITToolStripMenuItem.Name = "eXITToolStripMenuItem";
+            this.eXITToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.eXITToolStripMenuItem.Text = "EXIT";
+            this.eXITToolStripMenuItem.Click += new System.EventHandler(this.Exit_click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1460, 922);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // formMainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1484, 961);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "formMainWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "WORK_TIME_CONTROL";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem iMPORTNEWGRIDToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ToolStripMenuItem hELPToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aBOUTToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eXITToolStripMenuItem;
+
+    }
+}
+
---
+++ WORK_TIME_CONTROL.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace WORK_TIME_CTRL
+{
+    public partial class formMainWindow : Form
+    {
+        
+        public formMainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btopen_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+            try
+            {
+                OpenFileDialog openFileDialog1 = new OpenFileDialog();  //create openfileDialog Object
+                openFileDialog1.Filter = "XML Files (*.xml; *.xls; *.xlsx; *.xlsm; *.xlsb) |*.xml; *.xls; *.xlsx; *.xlsm; *.xlsb";//open file format define Excel Files(.xls)|*.xls| Excel Files(.xlsx)|*.xlsx| 
+                openFileDialog1.FilterIndex = 3;
+
+                openFileDialog1.Multiselect = false;        //not allow multiline selection at the file selection level
+                openFileDialog1.Title = "Open Text File-R13";   //define the name of openfileDialog
+                openFileDialog1.InitialDirectory = @"Desktop"; //define the initial directory
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)        //executing when file open
+                {
+                    string pathName = openFileDialog1.FileName;
+                    string fileName = System.IO.Path.GetFileNameWithoutExtension(openFileDialog1.FileName);
+                    DataTable tbContainer = new DataTable();
+                    string strConn = string.Empty;
+                    string sheetName = "Sheet1";
+
+                    FileInfo file = new FileInfo(pathName);
+                    if (!file.Exists) { throw new Exception("Error, file doesn't exists!"); }
+                    string extension = file.Extension;
+                    switch (extension)
+                    {
+                        case ".xls":
+                            strConn = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + pathName + ";Extended Properties='Excel 8.0;HDR=Yes;IMEX=1;'";
+                            break;
+                        case ".xlsx":
+                            strConn = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + pathName + ";Extended Properties='Excel 12.0;HDR=Yes;IMEX=1;'";
+                            break;
+                        default:
+                            strConn = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + pathName + ";Extended Properties='Excel 8.0;HDR=Yes;IMEX=1;'";
+                            break;
+                    }
+                    OleDbConnection cnnxls = new OleDbConnection(strConn);
+                    OleDbDataAdapter oda = new OleDbDataAdapter(string.Format("select * from [{0}$]", sheetName), cnnxls);
+                    oda.Fill(tbContainer);
+
+                    dataGridView1.DataSource = tbContainer;
+                    //dataGridView1.AutoResizeColumns();
+                    dataGridView1.EndEdit();
+                    dataGridView1.Columns.Add("ALERT1", "ALERT2");
+
+                    foreach (DataGridViewRow row in dataGridView1.Rows)
+                    {
+                        try
+                        {
+                            if ((Convert.ToInt32(row.Cells[0].Value) > 0 && (Convert.ToInt32(row.Cells[0].Value) < 31)))
+                            {
+                                if (row.Cells[1].Value.ToString() == "Niedziela i Święta" || row.Cells[1].Value.ToString() == "Wolne")
+                                {
+                                    try
+                                    {
+                                        row.Cells[12].Value = Convert.ToDateTime(row.Cells[3].Value) - Convert.ToDateTime(row.Cells[2].Value);// "UWAGA SWIETO";
+                                        row.Cells[12].Style.BackColor = Color.Red;
+                                    }
+                                    catch { }
+
+                                }
+                                else if (row.Cells[1].Value.ToString() == "Pracujący")
+                                {
+                                    try
+                                    {
+                                        if ((Convert.ToDateTime(row.Cells[2].Value) > Convert.ToDateTime("05:30") && Convert.ToDateTime(row.Cells[3].Value) < Convert.ToDateTime("14:30"))
+                                            || (Convert.ToDateTime(row.Cells[2].Value) > Convert.ToDateTime("13:30") && Convert.ToDateTime(row.Cells[3].Value) < Convert.ToDateTime("22:30")))
+                                        {
+                                            //row.Cells[12].Value = Convert.ToDateTime("08:00");
+                                            row.Cells[12].Value = Convert.ToDateTime(row.Cells[3].Value) - Convert.ToDateTime(row.Cells[2].Value);
+                                            row.Cells[12].Style.BackColor = Color.Green;
+                                        }
+                                        else if (row.Cells[2].Value == null || row.Cells[2].Value == DBNull.Value || String.IsNullOrWhiteSpace(row.Cells[2].Value.ToString()))
+                                        {
+                                            row.Cells[12].Style.BackColor = Color.HotPink;
+                                            row.Cells[12].Value = "!!!UWAGA!!!";
+                                        }
+                                        else
+                                        {
+                                            row.Cells[12].Value = row.Cells[12].Value = Convert.ToDateTime(row.Cells[3].Value) - Convert.ToDateTime(row.Cells[2].Value);//"!!NADGODZINY!!";
+                                            row.Cells[12].Style.BackColor = Color.Lime;
+                                        }
+                                    }
+                                    catch
+                                    {
+
+                                    }
+                                }
+                                if(Convert.ToDateTime(row.Cells[4].Value) < Convert.ToDateTime("08:00"))
+                                {
+                                    row.Cells[4].Style.BackColor = Color.Red;
+                                    row.Cells[4].Style.ForeColor = Color.Yellow;
+                                }
+                            }
+                        }
+                        catch { }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error!");
+            }
+        }
+
+        private void AboutMe_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("To jest na (prawie) szybko naszkryfany programik\ndo sprawdzania poprawności wypełnienia druku RCP\nAutor: Marcin Pierchała\nmarcin.pierchala@icloud.com");
+        }
+
+        private void Exit_click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Dzięki za skorzystanie z programiku :-)\nChcesz zamknąć program?",
+                      "Chcesz zamknąć program?", MessageBoxButtons.YesNo);
+            switch (dr)
+            {
+                case DialogResult.Yes:
+                    this.Close();
+                    break;
+                case DialogResult.No:
+                    break;
+            }
+        }       
+    }
+}
